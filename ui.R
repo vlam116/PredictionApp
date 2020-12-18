92905837f3660453a77e4090cf5b7cf07b6c85c5
vlam116 <vlam116@gmail.com>
2020-12-18 13:51:58 -0800

    removed old UI ideas

--- ui.R
+++ ui.R
@@ -1,52 +1,5 @@
 library(shiny);library(shinyWidgets);library(shinydashboard)
 
-# shinyUI(fluidPage(
-#   
-#   setBackgroundColor(
-#     color = c("#716B6B", "#999494"),
-#     gradient = "linear",
-#     direction = "bottom"
-#   ),  
-#   
-#     navbarPage("Navigation Menu",
-#                tabPanel("PredNextWord",
-#                       sidebarPanel(
-#                         tags$style(".well {background-color:#D1CACA;}"),
-#                         div("Welcome to my R Shiny Word Prediction App!", style = "color:blue",
-#                             align = "center"),
-#                         br(),
-#                         br(),
-#                         textInput("userInput", "To get started, type something here, 
-#                                           then a next word prediction along with a table of 
-#                                           next word candidates will appear below.
-#                                           You are free to type as many words as you want."),
-#                         br(),
-#                         htmlOutput("Prediction"),
-#                         br(),
-#                         tableOutput("p")
-#                       ),
-#                     mainPanel(
-#                       h1("Next Word Prediction Application", align = "center", style = "color:white"),
-#                       h3("Created by Vin Lam", align = "center", style = "color:white"),
-#                       h5("As a project for the JHU Data Science Specialization", align = "center",
-#                          style = "color:white"),
-#                       br(),
-#                       br(),
-#                       br(),
-#                       img(src = "cat.png", height = 144, width = 144)
-#                     )
-#                ),
-#                tabPanel("About",
-#                             textOutput("About")),
-#                tabPanel("Katz Back-off Algorithm",
-#                             textOutput("KBO")),
-#                tabPanel("Data Mining Process",
-#                             textOutput("DMP")),
-#                tabPanel("Further Reading",
-#                             textOutput("FR"))
-#     )
-# ))
-
 dashboardPage(
   skin = "purple",
   dashboardHeader(title = "Next Word Prediction App"),
@@ -60,12 +13,6 @@ dashboardPage(
     )
   ),
   dashboardBody(
-    # tags$head(tags$style(HTML(
-    #   "/* body */
-    #     .content-wrapper, .right-side {
-    #       background-color: #737373;
-    #     }"
-    # ))),
     tabItems(
       tabItem(tabName = "main",
         fluidRow(
